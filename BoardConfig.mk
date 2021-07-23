b270cd2545760d7b94ad03dcbd191f85a57d78e0
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-07-23 15:51:42 +0600

    mojito: Enabled updateable APEX
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Ic82d2aa67b550c8da86aec81136b64feae9447f6
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 BOARD_VENDOR := xiaomi
 DEVICE_PATH := device/xiaomi/mojito
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
