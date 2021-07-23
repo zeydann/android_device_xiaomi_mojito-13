0054416b1eee707e8f79dbfd9cd3fc59522b6c81
LuK1337 <priv.luk@gmail.com>
2021-07-24 00:51:54 +0600

    mojito: power: Switch to TARGET_POWERHAL_MODE_EXT
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I57d6d57c4ca853f89092b402649b2c3619cf0d50
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,9 @@ TARGET_OTA_ASSERT_DEVICE := mojito,sunny
 TARGET_BOARD_PLATFORM := sm6150
 BOARD_USES_QCOM_HARDWARE := true
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
