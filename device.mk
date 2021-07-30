c974ec3a2d2fa4faee745283d8fd44d14f8ab5a8
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-31 00:16:52 +0600

    mojito: Build multihal sensor
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I2b00a00ba017c986a631173771ca8cca8688b5c6
    [fiqri19102002: Always build multihal sensor]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- device.mk
+++ device.mk
@@ -362,6 +362,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal
+
+# Sensors configs
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
