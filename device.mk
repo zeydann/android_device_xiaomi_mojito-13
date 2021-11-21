1c960cec0ebd847018d7a235e804de7eca262b34
raghavt20 <raghavtandon2000@gmail.com>
2021-11-21 18:31:50 +0000

    mojito: parts: Import from xiaomi_sdm845-common
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- device.mk
+++ device.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
