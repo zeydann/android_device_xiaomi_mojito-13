979300c5a65c523b01bd12f921f5e3a10bda30d5
Georg Veichtlbauer <georg@vware.at>
2022-03-10 16:03:55 +0100

    mojito: Remove kernel CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,6 @@ BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += kpti=off
 
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := mojito_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mojito
 
