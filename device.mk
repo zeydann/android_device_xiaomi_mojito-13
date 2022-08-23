374cb0fd11cd4b0d537d21e1134939ab4dfd5f18
tarsin <yuanqingxiang233@163.com>
2022-08-23 14:08:55 +0530

    mojito: Make shim for com.qti.node.watermark.so
    Signed-off-by: Kujou Yuko <i@779.moe>
    Signed-off-by: Anush02198 <anush.4376@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- device.mk
+++ device.mk
@@ -443,6 +443,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Shims
+PRODUCT_PACKAGES += \
+    libwatermark_shim
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
