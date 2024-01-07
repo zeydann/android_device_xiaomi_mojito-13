PixelOS-Devices/device_xiaomi_sunny

mojito: Build custom sensors MultiHal
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

device.mk
@@ -441,7 +441,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal
+    android.hardware.sensors@2.1-service.xiaomi_mojito-multihal
 
 # Sensors configs
 PRODUCT_COPY_FILES += \

zeydann/android_device_xiaomi_mojito-13

Change-Id: I3c773ff7c3151e3b8e965dc4de3ce8ac7974c194

device.mk
@@ -427,7 +427,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal
+    android.hardware.sensors@2.1-service.mojito-multihal
 
 # Sensors configs
 PRODUCT_COPY_FILES += \