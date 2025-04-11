de437ff342c69cc6c8f0cd009f5e57da6e993c47
build-13 <173170056+build-13@users.noreply.github.com>
2025-04-11 23:37:03 +0700

    mojito: Drop unused drm packages

--- device.mk
+++ device.mk
@@ -200,9 +200,6 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
